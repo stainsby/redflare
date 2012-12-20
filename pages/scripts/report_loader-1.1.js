a90dd242f119c1292357fd0400ea2f7d1b3f9ab7
Sam Stainsby <sam@sustainablesoftware.com.au>
2012-12-21 09:03:20 +1000

    fixed loader script for unknown verisons - latest RE SVN servers no longer break page display

--- pages/scripts/report_loader-1.1.js
+++ pages/scripts/report_loader-1.1.js
@@ -14,41 +14,45 @@ function loadLatestReport() {
       _.keys(reports),
       function(key) {
         var report = reports[key];
-        playerCount = playerCount + report.clients;
-        serverCount++;
-        if (report.clients > 0) activeServerCount++;
-        report.lastSeen = '' + prettyDate(-Math.round(((new Date()).getTime() - report.reported)/1000));
-        var fullness = report.clients/report.maxClients;
-        if (fullness > 1) fullness = 1; // maxClients doesn't count admins
-        report.fullness = '' + Math.round(fullness*100) + '%';
-        report.gameMode = report.gameMode;
-        report.mutators = report.mutators.join(' ');
-        report.remaining = Math.round(report.timeLeft/60);
-        var playerNames = _.map(report.playerNames, function(name) {
-          return name.plain;
-        });
-        report.names = playerNames.join(', ');
-        var cssClasses = [];
-        if (report.clients == 0) {
-          cssClasses.push('empty');
-        } else if (report.clients == report.maxClients) {
-          cssClasses.push('full');
-        }
-        if (report.masterMode != 'open') {
-          cssClasses.push('notopen');
-        }
-        report.cssClasses = cssClasses.join(' ');
-        var heatColorHSL = [
-          180 - Math.round(fullness*180),
-          (15 + Math.round(fullness*85)) + '%',
-          (10 + Math.round(fullness*40)) + '%'
-        ];
-        if (fullness == 0) {
-          report.heatColor = '#161616';
-        } else if (fullness == 1) {
-          report.heatColor = '#f00';
-        } else {
-          report.heatColor = 'hsl(' + heatColorHSL.join(', ') + ')';
+        try {
+          playerCount = playerCount + report.clients;
+          serverCount++;
+          if (report.clients > 0) activeServerCount++;
+          report.lastSeen = '' + prettyDate(-Math.round(((new Date()).getTime() - report.reported)/1000));
+          var fullness = report.clients/report.maxClients;
+          if (fullness > 1) fullness = 1; // maxClients doesn't count admins
+          report.fullness = '' + Math.round(fullness*100) + '%';
+          report.gameMode = report.gameMode;
+          report.mutators = report.mutators.join(' ');
+          report.remaining = Math.round(report.timeLeft/60);
+          var playerNames = _.map(report.playerNames, function(name) {
+            return name.plain;
+          });
+          report.names = playerNames.join(', ');
+          var cssClasses = [];
+          if (report.clients == 0) {
+            cssClasses.push('empty');
+          } else if (report.clients == report.maxClients) {
+            cssClasses.push('full');
+          }
+          if (report.masterMode != 'open') {
+            cssClasses.push('notopen');
+          }
+          report.cssClasses = cssClasses.join(' ');
+          var heatColorHSL = [
+            180 - Math.round(fullness*180),
+            (15 + Math.round(fullness*85)) + '%',
+            (10 + Math.round(fullness*40)) + '%'
+          ];
+          if (fullness == 0) {
+            report.heatColor = '#161616';
+          } else if (fullness == 1) {
+            report.heatColor = '#f00';
+          } else {
+            report.heatColor = 'hsl(' + heatColorHSL.join(', ') + ')';
+          }
+        } catch(err) {
+          //console.log("WARNING: failed to load: ", key, "\nerror was:\n", err, "\nreport was:\n", report);
         }
         return report;
       }
