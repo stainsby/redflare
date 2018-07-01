e807e7a56a72c949f08735ffbddea2c47382b17f
Sam Stainsby <sam@stainsby.id.au>
2018-07-01 14:39:43 +1000

    precompile CSS; added PWS cache versioning; icon updates

--- pages/sw.js
+++ pages/sw.js
@@ -1,3 +1,5 @@
+var CACHE_NAME = 'redflare-7';
+
 self.addEventListener('notificationclick', function(event) {
   var action = event.action || null;
   event.waitUntil(self.clients.matchAll({type: 'window'}).then(function(clients) {
@@ -15,29 +17,35 @@ self.addEventListener('notificationclick', function(event) {
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('redflare').then(function(cache) {
+   caches.open(CACHE_NAME).then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/mobile.html',
-       '/index.html?utm_source=a2hs',
-       '/mobile.html?utm_source=a2hs',
-       '/favicon.ico',
-       '/less/elements-0.6.less',
-       '/less/theme-1.0.less',
-       '/less/main-1.0.less',
-       '/flags-css/flag-icon.min.css',
-       '/templates/listing_template-1.0.html',
-       '/js/underscore-1.3.1.min.js',
-       '/js/jquery-1.7.1.min.js',
-       '/js/less-1.3.0.min.js',
-       '/js/handlebars.-4.0.5.min.js',
-       '/js/pretty_date-1.0.js',
-       '/js/jquery.tablesorter-2.0.5b.min.js',
-       '/scripts/report_loader-1.2.js',
-       '/scripts/template_loader-1.0.js',
-       '/scripts/notifier-1.0.js',
-       '/fonts/ProstoOne-Regular.ttf'
+      '/',
+      '/index.html',
+      '/mobile.html',
+      '/index.html?utm_source=a2hs',
+      '/mobile.html?utm_source=a2hs',
+      '/favicon.ico',
+      '/css/main.css',
+      '/flags-css/flag-icon.min.css',
+      '/templates/listing_template-1.0.html',
+      '/js/underscore-1.3.1.min.js',
+      '/js/jquery-1.7.1.min.js',
+      '/js/less-1.3.0.min.js',
+      '/js/handlebars.-4.0.5.min.js',
+      '/js/pretty_date-1.0.js',
+      '/js/jquery.tablesorter-2.0.5b.min.js',
+      '/scripts/report_loader-1.2.js',
+      '/scripts/template_loader-1.0.js',
+      '/scripts/notifier-1.0.js',
+      '/fonts/ProstoOne-Regular.ttf',
+      '/img/bg.jpg',
+      '/icons/redflare_icon_192.png',
+      '/icons/redflare_icon_512.png',
+      '/icons/redflare_icon_tr_96.png',
+      '/icons/redflare_icon_tr_192.png',
+      '/icons/redflare_icon_tr_512.png',
+      '/icons/actions/bell-slash-o.png',
+      '/icons/actions/external-link.png',
      ]);
    })
  );
@@ -52,6 +60,21 @@ self.addEventListener('fetch', function(event) {
   );
 
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != CACHE_NAME;  // if an old cache ...
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);  // .. delete it
+        })
+      );
+    })
+  );
+});
+
 
 function cleanup() {
   // close any old notifications
